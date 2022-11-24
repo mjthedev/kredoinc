f46ad57f5c19d21e7448fb482d791705f5be590d
mjthedev <i@marrionjohnson.io>
2022-11-24 10:18:27 -0700

    added C# back-end api

---
+++ back-end/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+using Ops;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+
+app.MapGet("/api/category", () =>
+{
+
+
+    GetAllCategories();
+});
+
+//app.MapGet("/api/category/{id}", Ops.Operations.GetCategory);
+
+//app.MapGet("/api/product/{id}", Ops.Operations.GetProduct);
+
+
+app.Run();
+
---
+++ back-end/operations/Operations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+
+namespace Ops { 
+
+	public class Operations
+	{
+    public static GetAllCategories(string)
+		{
+            return "GetAllCategories method working";
+		}
+
+   public static void GetCategory(string[] args)
+    {
+        Console.WriteLine("GetCategories method working");
+    }
+
+    public static void GetProduct(string[] args)
+    {
+        Console.WriteLine("GetProduct method working");
+    }
+}
+
+}
